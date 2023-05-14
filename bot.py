58e85e1a62eaae027decba74bcdaf138e6a247b8
galoryzen <rjlopezgr@gmail.com>
2023-05-14 18:29:49 -0500

    feat: cierre de caja

--- bot.py
+++ bot.py
@@ -7,7 +7,40 @@ import sqlite3
 import re
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hello World!')
+    update.message.reply_text('Hola, soy el bot K-ching. Puedes enviarme una foto de tu transferencia y yo la guardaré en mi base de datos.')
+    
+    
+def cierre(update: Update, context: CallbackContext) -> None:
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    
+    username = update.message.from_user.username
+
+    #Get the sum of all amounts in the current date for the user sending the message
+    c.execute('''SELECT SUM(amount) FROM transferencia
+                    WHERE date(created_at) = date('now')
+                    AND username = ?
+                ''', (username,))
+    row = c.fetchone()
+    if row:
+        update.message.reply_text(f'Has recibido ${row[0]} pesos hoy.')
+    else:
+        update.message.reply_text('No has recibido dinero hoy.')
+
+    #Now display all the transactions for the current date and the user sending the message
+    c.execute('''SELECT amount, created_at FROM transferencia
+                    WHERE date(created_at) = date('now')
+                    AND username = ?
+                ''', (username,))
+    rows = c.fetchall()
+    
+    if rows:
+        update.message.reply_text('A continuación, el detalle de tus transferencias de hoy:')
+        for row in rows:
+            update.message.reply_text(f'Transferencia por ${row[0]} \nFecha de registro: {row[1]}')
+    
+    conn.close()
+    
 
 def handle_image(update: Update, context: CallbackContext) -> None:
     user = update.message.from_user
@@ -23,15 +56,14 @@ def handle_image(update: Update, context: CallbackContext) -> None:
         amount = float(amount.replace('.', ''))  # Remove '.' and convert to float
     else:
         amount = 0.0  # If no amount found, default to 0.0
-        
     
     save_user(user.username, text, amount)
-    update.message.reply_text(f'transferencia de ${amount} pesos?')
-
+    update.message.reply_text(f'Transferencia de ${amount} pesos registrada.')
 
 
 def handle_text(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Invalid message. Please send an image.')
+    
 
 def main() -> None:
     updater = Updater(token=config('TELEGRAM_TOKEN'))
@@ -39,12 +71,14 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("cierre", cierre))
     dispatcher.add_handler(MessageHandler(Filters.photo, handle_image))
     dispatcher.add_handler(MessageHandler(Filters.text, handle_text))
 
     updater.start_polling()
 
     updater.idle()
+    
 
 def save_user(phone_number: str, text: str, amount: float):
     conn = sqlite3.connect('data.db')
@@ -55,19 +89,22 @@ def save_user(phone_number: str, text: str, amount: float):
 
     conn.commit()
     conn.close()
+    
 
 def create_table():
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
 
     c.execute('''CREATE TABLE IF NOT EXISTS transferencia
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                  username TEXT(40),
-                  amount REAL,
-                  description TEXT)''')
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username    TEXT(40),
+                amount      REAL,
+                description TEXT,
+                created_at  datetime default CURRENT_TIMESTAMP not null)''')
 
     conn.commit()
     conn.close()
+    
 
 if __name__ == '__main__':
     create_table()
